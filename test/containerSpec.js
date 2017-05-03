530f6746c7fd56463fd6fc0c51cdee42c22969c9
lehren <loek-ehren@hotmail.com>
2017-05-03 11:44:33 +0200

    feat(container): add methods to get containers

---
+++ test/containerSpec.js
@@ -0,0 +1,100 @@
+import Connection from '../src/connection/connection';
+import ContainerHandler from '../src/container/container-controller';
+
+describe('Container', () => {
+  it('should not construct with invalid values', () => {
+    [0, '1', {}, [], true, false, null, undefined].map(v => {
+      expect(() => {
+        new ContainerHandler(v);
+      }).toThrowError('connection parameter of type "Connection" is required');
+    });
+  });
+
+  it('should construct with a connection object', () => {
+    const conn = new Connection('url');
+    const containerHandler = new ContainerHandler(conn);
+    expect(containerHandler.connection).toEqual(conn);
+  });
+
+  it('should not get a container if an invalid value is given', done => {
+    const conn = new Connection('url');
+    const containerHandler = new ContainerHandler(conn);
+    [0, {}, [], true, false, null, undefined].map(v => {
+      containerHandler.getContainer(v)
+        .then(fail)
+        .catch(error => {
+          expect(error.message).toEqual('id parameter of type "string" is required');
+        })
+        .then(done);
+    });
+  });
+
+  it('should get one container', done => {
+    const conn = new Connection('url');
+    const containerHandler = new ContainerHandler(conn);
+    const content = {
+      id: '1',
+      fillLevel: '50',
+      latitude: '32.4375838',
+      longitude: '53.353452',
+      lastUpdated: '2017-05-03T09:41:23+00:00'
+    };
+    const fakeResponse = new Response(JSON.stringify(content), {
+      status: 200,
+      headers: {
+        'Content-type': 'application/json; charset=utf-8'
+      }
+    });
+    spyOn(conn, 'get').and.callThrough();
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve(fakeResponse));
+
+    containerHandler.getContainer('1')
+      .then(result => {
+        expect(result).toEqual(content);
+        expect(conn.get).toHaveBeenCalledTimes(1);
+        expect(conn.get).toHaveBeenCalledWith('/containers/container/1');
+      })
+      .catch(fail)
+      .then(done);
+  });
+
+  it('should get multiple containers', done => {
+    const conn = new Connection('url');
+    const containerHandler = new ContainerHandler(conn);
+    const content =
+      [
+        {
+          id: '1',
+          fillLevel: '50',
+          latitude: '32.4375838',
+          longitude: '53.353452',
+          lastUpdated: '2017-05-03T09:41:23+00:00'
+        },
+        {
+          id: '2',
+          fillLevel: '34',
+          latitude: '87.453423',
+          longitude: '21.346552',
+          lastUpdated: '2017-05-03T09:41:23+00:00'
+        }
+      ];
+
+    const fakeResponse = new Response(JSON.stringify(content), {
+      status: 200,
+      headers: {
+        'Content-type': 'application/json; charset=utf-8'
+      }
+    });
+    spyOn(conn, 'get').and.callThrough();
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve(fakeResponse));
+
+    containerHandler.getContainers()
+      .then(result => {
+        expect(result).toEqual(content);
+        expect(conn.get).toHaveBeenCalledTimes(1);
+        expect(conn.get).toHaveBeenCalledWith('/containers/container');
+      })
+      .catch(fail)
+      .then(done);
+  });
+});

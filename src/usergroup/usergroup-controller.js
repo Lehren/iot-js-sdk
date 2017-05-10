998f223f3080498ad98ab3d537628e081e7bd9ac
lehren <loek-ehren@hotmail.com>
2017-05-10 11:43:05 +0200

    feat(usergroupcontroller): add controller for usergroup
    Users can now subscribe to containers with their email.

---
+++ src/usergroup/usergroup-controller.js
@@ -0,0 +1,21 @@
+import Connection from '../connection/connection';
+
+export default class UsergroupController {
+  constructor(connection) {
+    if (!(connection instanceof Connection)) {
+      throw new Error('connection parameter of type "Connection" is required');
+    }
+    this.connection = connection;
+  }
+
+  postUsergroup(containerId, email) {
+    if (typeof containerId !== 'string') {
+      return Promise.reject(new Error('containerId parameter of type "string" is required'));
+    }
+    if (typeof email !== 'string') {
+      return Promise.reject(new Error('email parameter of type "string" is required'));
+    }
+    const data = {email};
+    return this.connection.post(data, '/containers/usergroup/' + containerId);
+  }
+}

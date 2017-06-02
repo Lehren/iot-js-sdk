45c9b9a1c83a9b0fcfd88e84bca78071ddc27ac9
lehren <loek-ehren@hotmail.com>
2017-06-02 16:40:49 +0200

    Added historyhandler to exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Connection from './connection/connection';
 import ContainerController from './container/container-controller';
 import FillLevelController from './fill-level/fill-level-controller';
+import HistoryController from './history/history-controller';
 
 export {
   Connection,
   ContainerController,
-  FillLevelController
+  FillLevelController,
+  HistoryController
 };
